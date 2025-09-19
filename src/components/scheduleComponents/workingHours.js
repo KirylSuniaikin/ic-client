fae9aaf676f888796c4908244b4fb7dc978d631a
KirylSuniaikin <93624218+KirylSuniaikin@users.noreply.github.com>
2025-09-19 17:12:44 +0200

    Update workingHours.js

--- src/components/scheduleComponents/workingHours.js
+++ src/components/scheduleComponents/workingHours.js
@@ -4,6 +4,6 @@ export const workingHours = {
     "Wednesday": ["11:30", "00:00"],
     "Thursday": ["11:30", "01:30"],
     "Friday": ["13:30", "01:30"],
-    "Saturday": ["08:30", "00:00"],
-    "Sunday": ["13:30", "00:00"],
+    "Saturday": null,
+    "Sunday": null,
 };
