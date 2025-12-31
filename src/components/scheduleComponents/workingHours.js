094a6fab5ce0ebdfee141f2983554ac24f05c3b6
Andrew Suniaikin <andrewinschool@gmail.com>
2025-12-31 17:54:09 +0100

    Working Hours Update

--- src/components/scheduleComponents/workingHours.js
+++ src/components/scheduleComponents/workingHours.js
@@ -1,7 +1,7 @@
 export const workingHours = {
     "Monday": ["14:00", "23:59"],
     "Tuesday": ["14:00", "23:59"],
-    "Wednesday": ["14:00", "23:59"],
+    "Wednesday": ["14:00", "21:59"],
     "Thursday": ["15:30", "01:30"],
     "Friday": ["15:30", "01:30"],
     "Saturday": ["14:00", "23:59"],
